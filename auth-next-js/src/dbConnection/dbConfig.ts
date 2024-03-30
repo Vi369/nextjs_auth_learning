ec3e0a1e40093f359cdf7bc4c785c196763af10c
Vikas singh varma <Vikashverma3001@gmail.com>
2024-03-30 20:17:59 +0530

    user modeling and db connection

---
+++ auth-next-js/src/dbConnection/dbConfig.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose"
+
+export async function connectDb(){
+    try {
+        mongoose.connect(process.env.MONGO_URL!)
+        const connection = mongoose.connection
+        connection.on("connected", (err)=>{
+            console.log("Mongo DB is connected Error:", err);
+        })
+
+        connection.on("error", ()=>{
+            console.log(" mongo DB Connection Failed");
+            process.exit(1);
+        })
+        
+    } catch (error) {
+        console.log("Something Went wrong while connecting DB:");
+        console.log(error);
+    }
+}
---
+++ auth-next-js/src/models/user.model.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, "Username is required"],
+        unique: true
+    },
+    email: {
+        type: String,
+        required: [true, "email is required"],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, "password is required"],
+    },
+    isVerified:{
+        type: Boolean,
+        default: false
+    },
+    isAdmin:{
+        type: Boolean,
+        default: false
+    },
+    forgotPasswordToken: String,
+    forgotPasswordTokenExpiry: Date,
+    verifyToken: String,
+    verifyTokenExpiry: Date
+    
+})
+
+const User = mongoose.model.users ||  mongoose.model("users", userSchema)
+
+export default User 
+
