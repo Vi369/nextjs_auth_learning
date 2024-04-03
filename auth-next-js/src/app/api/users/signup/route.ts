4d21860f8bf27a3a6410af0aa8a9b5dfa5b302cf
Vikas singh varma <Vikashverma3001@gmail.com>
2024-04-03 18:44:00 +0530

    signup route done and sendMail configuration

---
+++ auth-next-js/src/app/api/users/signup/route.ts
@@ -0,0 +1,54 @@
+import { connectDb } from "@/dbConnection/dbConfig"; 
+import User from "@/models/user.model";
+import { NextRequest, NextResponse } from 'next/server'
+import bcryptjs from 'bcryptjs'
+import { sendEmail } from '@/helpers/mailer'
+
+connectDb()
+
+export async function POST(request: NextRequest){
+    try {
+        const reqBody = await request.json()
+        const {username, email, password} = reqBody;
+        // TODO: validation 
+        console.log(reqBody)
+
+        const user = await User.findOne({email})
+
+        if(user){
+            return NextResponse.json({error: "User already exist"}, {status: 400})
+        }
+
+        const salt = await bcryptjs.genSalt(10);
+
+        const hashedPassword = bcryptjs.hash(password, salt)
+
+        const newUser = new User({
+            username,
+            email,
+            password: hashedPassword
+        })
+
+        const saveUser = await newUser.save()
+
+        console.log("saved user",saveUser)
+
+        // send verification email 
+
+        await sendEmail({email, emailType: "VERIFY", userId: saveUser._id})
+
+        saveUser.password = undefined;
+        return NextResponse.json({
+            message: "User Registred successfully",
+            success: true,
+            saveUser
+        })
+    } catch (error: any) {
+        return NextResponse.json({
+            error: error.message
+        },
+        {
+            status: 500
+        })
+    }
+}
---
+++ auth-next-js/src/helpers/mailer.ts
@@ -0,0 +1,51 @@
+import User from '@/models/user.model';
+import nodemailer from 'nodemailer'
+import bcryptjs from 'bcryptjs'
+export  const sendEmail = async({email, emailType, userId}:any) =>{
+
+
+
+    
+    try {
+        // uuid library ka use kar sakte hai special character ni aayenge 
+        const hashedToken = await bcryptjs.hash(userId.toString(),10)
+
+        if(emailType ==="VERFIY"){
+            await User.findByIdAndUpdate(userId, {
+                verifyToken: hashedToken,
+                verifyTokenExpiry: Date.now() + 3600000
+            })
+        } else if (emailType === "RESET"){
+            await User.findByIdAndUpdate(userId, {
+                forgotPasswordToken: hashedToken,
+                forgotPasswordTokenExpiry: Date.now() + 3600000
+            })
+        }
+
+        var transport = nodemailer.createTransport({
+            host: "sandbox.smtp.mailtrap.io",
+            port: 2525,
+            auth: {
+              user: "cb72ee18b247e7", // env variable me hona chahiye
+              pass: "55c7704ba3ead8"
+            }
+          });
+    
+        const mailOptions = {
+            from: '',
+            to: email,
+            subject: emailType === "VERIFY" ? "Verify your email": "Reset your password",
+            html: `<p>
+            Click <a href = "${process.env.DOMAIN}/verifyemail?token=${hashedToken}">here to ${emailType ==="VERFIY"? "VERFIY YOUR EMAIL": "RESET YOUR PASSWORD"} or copy or paste the link below in browser.
+            <br> ${process.env.DOMAIN}/verifyemail?token=${hashedToken}
+            </p>`
+        }
+
+        const mailResponce = await transport.sendMail(mailOptions)
+
+        return mailOptions
+
+    } catch (error:any) {
+        throw new Error(error.message)
+    }
+}
