b6082194a4fb9632c4ad614b9f9119900868cdc6
Vikas singh varma <Vikashverma3001@gmail.com>
2024-04-04 19:55:20 +0530

    bug fix

--- auth-next-js/src/models/user.model.js
+++ auth-next-js/src/models/user.model.js
@@ -30,7 +30,7 @@ const userSchema = new mongoose.Schema({
     
 })
 
-const User = mongoose.model.users ||  mongoose.model("users", userSchema)
+const User = mongoose.models.users ||  mongoose.model("users", userSchema)
 
 export default User 
 
