625ceb0fea74d5e28b9ee0aee4bb24cfe8548a00
Vikas singh varma <Vikashverma3001@gmail.com>
2024-04-10 20:01:28 +0530

    bug resolve

--- auth-next-js/src/app/api/users/logout/route.ts
+++ auth-next-js/src/app/api/users/logout/route.ts
@@ -1,24 +1,17 @@
-import { connectDb } from "@/dbConnection/dbConfig";
 import { NextRequest, NextResponse } from 'next/server';
 
-connectDb();
-
-export async function GET(request: NextRequest) {
+export async function GET() {
     try {
         const response = NextResponse.json({
             message: "Logout successfully",
             success: true
         });
 
-        response.cookies.set("token", "", {
-            httpOnly: true,
-            expires: new Date(0)
+        response.cookies.set("token", "", 
+        { httpOnly: true, expires: new Date(0) 
         });
 
-        return NextResponse.json({
-            message: "User logout successfully",
-            success: true
-        }, { status: 200 });
+        return response
     } catch (error:any) {
         return NextResponse.json({ error: error.message }, { status: 500 });
     }
